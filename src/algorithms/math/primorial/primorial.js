dbad8ffb070a226a39c8385074379ed8bd74d045
Iman Rousta <imanrousta@gmail.com>
2021-05-16 20:44:00 +0430

    Adding primorial and is prime

---
+++ src/algorithms/math/primorial/primorial.js
@@ -0,0 +1,22 @@
+function primorial(n) {
+	let primeArray = [];
+
+	for (let i = 2; i <= n; i++) {
+		let flag = 0;
+
+		for (let j = 2; j < i; j++) {
+			if (i % j == 0) {
+				flag = 1;
+				break;
+			}
+		}
+
+		if (i > 1 && flag == 0) {
+			primeArray.push(i);
+		}
+	}
+
+	return primeArray.reduce( (a, b) => a * b );
+}
+
+
