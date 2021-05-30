85abc2132305237bca3e3d05e5f34903ad6b0848
Iman Rousta <imanrousta@gmail.com>
2021-05-30 14:33:48 +0430

    Adding MBE loss function

--- src/algorithms/deep learning/Loss Functions/meanBiasError.js
+++ src/algorithms/deep learning/Loss Functions/meanBiasError.js
@@ -1,12 +1,12 @@
-function mae(prediction, target) {
+function mbe(prediction, target) {
 	let sum = 0;
 	let ans = 0;
 	let targetLength = target.length;
 
-	for(let i = 0; i < targetLength; i++ ) {
+	for (let i = 0; i < targetLength; i++) {
 		let y = target[i];
 		let yHat = prediction[i];
-		sum += Math.abs(y - yHat);
+		sum += (y - yHat);
 	}
 
 	ans = sum / targetLength;
