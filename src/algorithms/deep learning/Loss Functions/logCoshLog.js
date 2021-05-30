a78aa035d3bbd8cb4dab890003f1d3dcdf0ca336
Iman Rousta <imanrousta@gmail.com>
2021-05-30 14:44:08 +0430

    Adding LCL loss function

---
+++ src/algorithms/deep learning/Loss Functions/logCoshLog.js
@@ -0,0 +1,14 @@
+function lcl(prediction, target) {
+	let sum = 0;
+	let ans = 0;
+	let targetLength = target.length;
+
+	for (let i = 0; i < targetLength; i++) {
+		let y = target[i];
+		let yHat = prediction[i];
+		sum += Math.log(Math.cosh(yHat - y));
+	}
+
+	ans = sum / targetLength;
+	return ans;
+}
