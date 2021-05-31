5ad0b4f0b1d3ef62989db33fd50dfc7ee9b5d479
Iman Rousta <imanrousta@gmail.com>
2021-05-31 14:00:00 +0430

    Adding BCE loss function

---
+++ src/algorithms/deep learning/Loss Functions/binaryCrossEntropy.js
@@ -0,0 +1,14 @@
+function bce(array) {
+	let sum = 0;
+	let ans = 0;
+	let targetLength = target.length;
+
+	for (let i = 0; i < targetLength; i++) {
+		let y = target[i];
+		let yHat = prediction[i];
+		sum += y * Math.log(yHat) + (1 - y) * Math.log(1 - yHat);
+	}
+
+	ans = -sum / targetLength;
+	return ans;
+}
